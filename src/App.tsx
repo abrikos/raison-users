eaa1e1d2324c6a616634a7a1bf70ae8ab1f4057d
abrikos <5422791+abrikos@users.noreply.github.com>
2021-10-27 08:23:53 +0900

    Assign values to User object by keys

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,39 @@ import axios from "axios";
 
 function App() {
     const [list, setList] = useState<[]>([]);
+    class IUser {
+        ID: number = 0;
+        Name: string = '';
+        Age: number = 0;
+        Phone: string = '';
+        "E-mail": string = '';
+    }
+
+    interface IData {
+        field: string,
+        value: number | string
+    }
+
+
     useEffect(() => {
         axios.get('https://frontend-test.netbox.ru/')
             .then(res => {
-                setList(res.data)
+                setList(res.data.map((row:IData[])=>getUser(row)))
             })
     }, [])
 
-    class IUser {
-        id: number = 0;
-        name: string = '';
-        age: number = 0;
-        phone: string = '';
-        email: string = ''
-    }
-
     const User = new IUser();
     const fields: Array<string> = Object.keys(User)
-    function getUser(item:Array<Object>){
-        const user = {}
-        for(const field of fields){
-            console.log(field)
-            //user[field] = item.filter((i:IData)=>i.field===field)
-        }
-    }
+    const setKeyValue = <T extends object, U extends keyof T>(obj: T, value:any) => (key: U) => obj[key] = value;
 
-    function parseUser(item:[], field:string):IUser{
-        const user:IUser = new IUser()
-        //TODO transform array to IUser
-        //user[field] = 'aaa';
-        return user;
+    function getUser(item: Array<IData>) {
+        let user:{[key:string]: any} =  new IUser();
+        for (const field in user) {
+            const data: IData = item.find((i: IData) => i.field === field) as IData;
+            setKeyValue(user, data.value)(field)
+
+        }
+        return user
     }
 
     return (
@@ -41,16 +44,12 @@ function App() {
             <table>
                 <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Phone</th>
-                    <th>Email</th>
+                    {fields.map(field => <th key={field}>{field}</th>)}
                 </tr>
                 </thead>
                 <tbody>
                 {list.map((item, i) => <tr key={i}>
-                    {fields.map(field=><td key={field}>{JSON.stringify(item)}</td>)}
+                    {fields.map(field => <td key={field}>{item[field]}</td>)}
                 </tr>)}
                 </tbody>
             </table>
